e885f31e56c6d2957337185b7e99f1eaa5869793
omar <omar@tzst.com>
2024-04-12 23:50:56 +0200

    confige database

--- Server/config/db.js
+++ Server/config/db.js
@@ -8,3 +8,14 @@ const connectDB = async () => {
   await mongoose.connect(db).then(() => {});
 };
 module.exports = connectDB;
+
+const mongoose = require("mongoose");
+
+const config = require("config");
+
+const db = config.get("mongoConnectionString");
+
+const connectDB = async () => {
+  await mongoose.connect(db).then(() => {});
+};
+module.exports = connectDB;
