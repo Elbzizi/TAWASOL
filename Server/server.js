2b39bbd46efa69df9af711759d319597169e1af3
omar <omar@tzst.com>
2024-04-12 20:07:32 +0200

    create server side node js

--- Server/server.js
+++ Server/server.js
@@ -1,7 +1,11 @@
-const express = reauire("express");
+const express = require("express");
 
 const app = express();
 
-app.listnear(3000, () => {
-  console.log("start server a port 3000");
+app.get("/", (req, res) => {
+  res.send("<h1>hello world im server</h1>");
+});
+const port = process.env.PORT || 3005;
+app.listen(port, () => {
+  console.log(`start server a port ${port}`);
 });
