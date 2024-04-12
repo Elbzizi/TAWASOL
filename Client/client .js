85fdd184d181bd9120b7f8fa007c4c06e86ac818
omar <omar@tzst.com>
2024-04-12 23:28:00 +0200

    create server side node js

--- Client/client .js
+++ Client/client .js
@@ -1,11 +1,11 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8" />
-  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <title>Document</title>
-</head>
-<body>
-  
-</body>
-</html>
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("<h1>hello world im server</h1>");
+});
+const port = process.env.PORT || 3005;
+app.listen(port, () => {
+  console.log(`start server a port ${port}`);
+});
