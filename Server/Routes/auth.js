d9eda423a24f2b0f6a9c3450f51fde0aa68f46d8
omar <omar@tzst.com>
2024-04-13 18:01:04 +0200

    register user

--- Server/Routes/auth.js
+++ Server/Routes/auth.js
@@ -1,9 +1,20 @@
 const express = require("express");
 const router = express.Router();
+const { check, validationResult } = require("express-validator");
 
-router.get("/", (req, res) => {
-  res.send("Hello, API Auth!");
-});
+router.post(
+  "/",
+  check("name", "Name is required").notEmpty(),
+  check("email", "Invalid email").isEmail(),
+  check("password", "Password must be between 6 and 12 characters").isLength({min: 6}),
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    const { name, email, password } = req.body;
+    res.send("Added data successfully");
+  }
+);
 
 module.exports = router;
-
