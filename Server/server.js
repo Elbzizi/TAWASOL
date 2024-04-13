51a9e904f4c8cf4699ac28534e01640c599022b6
omar <omar@tzst.com>
2024-04-13 16:49:05 +0200

    partede server

--- Server/server.js
+++ Server/server.js
@@ -1,20 +1,21 @@
 const express = require("express");
 const app = express();
 const db = require("./config/db");
-const auth = require("./Routes/auth");
-const user = require("./Routes/user");
-const poste = require("./Routes/poste");
-const profile = require("./Routes/profile");
+const authRoutes = require("./Routes/auth.js");
+const userRoutes = require("./Routes/user");
+const posteRoutes = require("./Routes/poste");
+const profileRoutes = require("./Routes/profile");
 
 db();
 
 app.get("/", (req, res) => {
   res.send("<h1>hello world im server</h1>");
 });
-app.use("api/auth", auth);
-app.use("api/user", user);
-app.use("api/poste", poste);
-app.use("api/profile", profile);
+app.use("/api/user", userRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/user", userRoutes);
+app.use("/api/poste", posteRoutes);
+app.use("/api/profile", profileRoutes);
 const port = process.env.PORT || 3005;
 app.listen(port, () => {
   console.log(`start server a port ${port}`);
